0943bb0bc49981f04d9260ae343a430bc97b4692
IMAD-Majid <imadvlp0@gmail.com>
2023-10-31 13:45:04 +0100

    Fixed caching file paths

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,60 @@
+const CACHE_VERSION = "2023-10-31 1:40 PM";
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('IHIIT').then(cache => {
+    caches.open(CACHE_VERSION).then(cache => {
       return cache.addAll([
         '/',
-        '/*',
-        '/gifs/*',
-        '/svg icons/*'
+        '/index.html',
+        '/style.css',
+        '/main.js',
+        '/icon.svg',
+        '/manifest.json',
+        '/service-worker.js',
+
+        '/svg icons/favorite_white_24dp.svg',
+        '/svg icons/play_circle_filled_white_24dp.svg',
+
+        '/gifs/Bicycle Crunches.gif',
+        '/gifs/Burpees.gif',
+        '/gifs/Diamond Push-ups.gif',
+        '/gifs/Explosive Push-ups.gif',
+        '/gifs/High Knees.gif',
+        '/gifs/Jump Squats.gif',
+        '/gifs/Jumping Jacks.gif',
+        '/gifs/Jumping Lunges.gif',
+        '/gifs/Jumping Squats.gif',
+        '/gifs/Leg Raises.gif',
+        '/gifs/Mountain Climbers.gif',
+        '/gifs/Plank Hold.gif',
+        '/gifs/Plank Jacks.gif',
+        '/gifs/Push-ups.gif',
+        '/gifs/Russian Twists.gif',
+        '/gifs/Shoulder Taps.gif',
+        '/gifs/Side Plank.gif',
+        '/gifs/Sit-ups.gif',
+        '/gifs/Superman Plank.gif',
+        '/gifs/Wall Sit.gif',
       ]);
     })
   );
 });
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 
 self.addEventListener('fetch', event => {
   event.respondWith(
